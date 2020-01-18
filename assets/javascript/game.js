6174dcd14e673f5f0184fc2c2fadc703ae147c14
hgowton <hgowton@gmail.com>
2020-01-18 15:08:34 -0500

    functions for start of game and generation of buttons by animal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,37 +7,83 @@ var totalScore = 0;
 var wins = 0;
 var loses = 0;
 
-//Generate random number for goal
-var goalRandom = Math.floor(Math.random() * (120 - 19));
-document.getElementById("goalRandom").innerHTML="Current Goal: " + goalRandom
+//variables never appear on screen for user
+var dogRandom = "";
+var catRandom = "";
+var turtleRandom = "";
+var birdRandom = "";
 
-//??? Not sure if needed (Crystal Game #07) Current Goal and target number to allow
-var dogRandom = Math.floor(Math.random() * (12 - 2));
-console.log("dog increase: " + dogRandom);
+function startGame () {
+    //Generate random number for goal
+    goalRandom = Math.floor(Math.random() * 100) + 19;
+
+    //Generate random number for each animal
+    dogRandom = buttonGenerator();
+    catRandom = buttonGenerator();
+    turtleRandom = buttonGenerator();
+    birdRandom = buttonGenerator();
+
+    console.log("Dog: " + dogRandom);
+    console.log("Cat: " + catRandom);
+    console.log("Turtle: " + turtleRandom);
+    console.log("Bird: " + birdRandom);
 
 
-//Responding to clicks on animal images
-$(".animal-dog").on("click", function() {
-    currentRoundScore += dogRandom;
+    //Prints goal for game
+    document.getElementById("goalRandom").innerHTML="Your Goal: " + goalRandom
+};
 
-    //Checks if round score matches goal
+//Creates random number for each animal
+function buttonGenerator () {
+    return Math.floor(Math.random() * 12) + 1;
+};
+
+
+//What happens when you click an animal
+    $(".animal-dog").on("click", function() {
+        currentRoundScore += dogRandom;
+        gameProgress();
+    });
+
+    $(".animal-cat").on("click", function() {
+        currentRoundScore += catRandom;
+        gameProgress();
+    });
+
+    $(".animal-turtle").on("click", function() {
+        currentRoundScore += turtleRandom;
+        gameProgress();
+    });
+
+    $(".animal-bird").on("click", function() {
+        currentRoundScore += birdRandom;
+        gameProgress();
+    });
+
+//Determines won, lost, or game in progress
+function gameProgress() {
     if (goalRandom === currentRoundScore) {
         wins++;
-        
+        currentRoundScore = 0;
+        alert ("You Won!");
+
         //logic where currentRoundScore adds to totalScore
         totalScore += currentRoundScore;
 
-        document.getElementById("wins").innerHTML="Wins: " + wins
+        document.getElementById("wins").innerHTML="Wins: " + wins;
+        startGame();
+    }
+    else if (goalRandom <= currentRoundScore) {
+        alert("You Lost!");
+        loses++;
+        currentRoundScore = 0;
+        document.getElementById("loses").innerHTML="Loses: " + loses;
+        startGame();
+
     }
 
         //NEED TO ADD --- logic where if currentRoundScore is greater than goal == game restarts and loses increments
         console.log(currentRoundScore);
 
-    document.getElementById("totalScore").innerHTML="Current Score: " + currentRoundScore
-});
-
-
-
-
-
-
+    document.getElementById("totalScore").innerHTML="Current Score: " + currentRoundScore;
+};
